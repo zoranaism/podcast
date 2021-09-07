1347c72a238982be7feefeeef7084a5bc9c84702
tijana <tijanagojak1@gmail.com>
2021-09-07 20:29:39 +0200

    create EP4

---
+++ src/pages/AllEpisodes/Season1/EP4.js
@@ -0,0 +1,31 @@
+import React from "react";
+import EpisodeComponent from "../../../components/EpisodePage.component";
+
+
+export default function Episode4() {
+  return (
+    <EpisodeComponent
+    episodeName='Pilot Episodes | Episode 4'
+    host="radina"
+    guest="Zsolt Argyelan"
+    guestLink="https://www.youtube.com/watch?v=y3GKavuTrRk"
+    spotifyLink="https://open.spotify.com/episode/2LRMrXjomKiuIE6VcCEH34"
+    youtubeLink="https://www.youtube.com/watch?v=XKd36gUUrOQ&t=2975s"
+    applePodcastsLink="https://podcasts.apple.com/rs/podcast/pilot-episode-4-guest-zsolt-argyelan-host-radina-dimcheva/id1556200413?i=1000527570484"
+    sponsor="ketz"
+    spotifyPlayerLink="https://open.spotify.com/embed/episode/2Ai9cl6ubhXmUi08MR8zYk"
+    description1="Our third episode guest is Aleksandra Denda, an internationally recognized performer, songwriter, music educator, and co-founder of a multinational vocal group ROSA."
+    description2="In this chat with host Megan, Aleksandra is introspective about her experience of moving and living abroad for college, being an international musician, enduring one year of silence due to Covid, as well as balancing her two biggest passions, music education and performing."
+    description3='Through the conversation, we get an insight into her mindset and how she keeps her inner peace during these times of worldwide musical stagnation, her EP ‘Dreamer’ and new single "Between The Lines".'
+    description4=""
+    guestsImagePath="img/episodes/guests/aleksandra-denda.webp"
+    prevEpisode='Episode 2: HunBjørn'
+    prevEpisodePath="episode-2-hunbjorn.html"
+    prevEpisodeImgPath="img/episodes/episodes-cover/pilot/episode-2-cover.webp"
+    nextEpisodePath="episode-4-zsolt-argyelan.html"
+    nextEpisodeImgPath="img/episodes/episodes-cover/pilot/episode-4-cover.webp"
+    nextEpisode='Episode 4: Zsolt Argyelan'
+  
+  /> 
+  );
+}
