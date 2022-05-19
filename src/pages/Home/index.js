d2170d6092f6bcce2eae5657c03974cb5f9facbf
Andrija <kostic.andrija00@gmail.com>
2022-05-19 16:33:40 +0200

    sponsor grid col issue

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -322,7 +322,7 @@ export default function Home(props) {
             <h1 className="nj-section-header-title">stellar partners</h1>
           </div>
           <div className="row row-cols-1 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-5">
-            <div className="col col-md-5 mb-4">
+            <div className="col mb-4">
               <div
                 className="sponsor-img"
                 data-anim-queue="1"
@@ -337,7 +337,7 @@ export default function Home(props) {
                 </a>
               </div>
             </div>
-            <div className="col col-md-5 mb-4">
+            <div className="col mb-4">
               <div
                 className="sponsor-img"
                 data-anim-queue="2"
@@ -356,7 +356,7 @@ export default function Home(props) {
                 </a>
               </div>
             </div>
-            <div className="col col-md-5 mb-4">
+            <div className="col mb-4">
               <div
                 className="sponsor-img"
                 data-anim-queue="3"
@@ -375,7 +375,7 @@ export default function Home(props) {
                 </a>
               </div>
             </div>
-            <div className="col col-md-5 mb-4">
+            <div className="col mb-4">
               <div
                 className="sponsor-img"
                 data-anim-queue="4"
@@ -394,7 +394,7 @@ export default function Home(props) {
                 </a>
               </div>
             </div>
-            <div className="col col-md-5 mb-4">
+            <div className="col mb-4">
               <div
                 className="sponsor-img"
                 data-anim-queue="5"
