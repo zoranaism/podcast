3072312b15e16d8dd76f9318ce2974ee9a813132
zorana.lazarevic <zorana.lazarevic@basic-fit.com>
2022-08-04 23:12:14 +0200

    first commit, reformat code

--- src/pages/Volunteer/index.js
+++ src/pages/Volunteer/index.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Modal from '../../components/Modal';
-import { createPortal } from 'react-dom';
-import useAnalyticsEventTracker from '../../useAnalyticsEventTracker';
+import Modal from "../../components/Modal";
+import { createPortal } from "react-dom";
+import useAnalyticsEventTracker from "../../useAnalyticsEventTracker";
 
 const Volunteer = () => {
-
   const gaEventTracker = useAnalyticsEventTracker("Volunteer");
 
   const [galleryImages] = useState([
@@ -33,51 +32,71 @@ const Volunteer = () => {
 
   const closeModalHandler = () => {
     setIsModalShown(false);
-  }
+  };
 
   const imageClickHandler = (index) => {
-    setIsModalShown(true)
+    setIsModalShown(true);
     setIndex(index);
-  }
+  };
 
   const prevHandler = () => {
-    setIndex(prev => {
-      if(prev === 0) {
-        return galleryImages.length-1;
+    setIndex((prev) => {
+      if (prev === 0) {
+        return galleryImages.length - 1;
       } else {
         return prev - 1;
       }
-    })
-  }
+    });
+  };
 
   const nextHandler = () => {
-    setIndex(prev => {
-      if(prev === galleryImages.length-1) {
+    setIndex((prev) => {
+      if (prev === galleryImages.length - 1) {
         return 0;
       } else {
         return prev + 1;
       }
-    })
-  }
+    });
+  };
 
   return (
     <React.Fragment>
-    {isModalShown && createPortal(<Modal prev={prevHandler} next={nextHandler} closeModal={closeModalHandler} image={galleryImages[index]}  />, document.getElementById('modal-root'))}
-        <div id="nj-preloader-wrapper">
-            <div className="nj-preloader-inner">
-                <div className="nj-preloader-percentage">0</div>
-            </div>
-    </div>
-    <div className="section nj-hero" id="nj-volunteer">
-      <div className="nj-section-header">
-        <h1 className="nj-section-header-title mt-5">Become a volunteer</h1>
+      {isModalShown &&
+        createPortal(
+          <Modal
+            prev={prevHandler}
+            next={nextHandler}
+            closeModal={closeModalHandler}
+            image={galleryImages[index]}
+          />,
+          document.getElementById("modal-root")
+        )}
+      <div id="nj-preloader-wrapper">
+        <div className="nj-preloader-inner">
+          <div className="nj-preloader-percentage">0</div>
+        </div>
       </div>
-      {/* WHY JOIN US SECTION */}
-      <div className="section nj-hero mt-3">
-      <div className="container">
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-      <h2>why would you join?</h2>
-      <div className="row icons-row">
+      <div className="section nj-hero" id="nj-volunteer">
+        <div className="nj-section-header">
+          <h1 className="nj-section-header-title mt-5">Become a volunteer</h1>
+        </div>
+        {/* WHY JOIN US SECTION */}
+        <div className="section nj-hero mt-3">
+          <div className="container">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <h2>why would you join?</h2>
+            <div className="row icons-row">
               <div className="icon-holder">
                 <span>
                   <img src="img/icons/idea.webp" alt="Education" />
@@ -102,70 +121,79 @@ const Volunteer = () => {
                 </span>
                 <h3>Fun and joy</h3>
               </div>
-      </div>
-      
-      </div>
-      </div>
-      {/* WHAT WE DO SECTION */}
-      <div className="section nj-light-section" 
-      data-scroll-to-top-style="dark"
-      data-cursor-style="dark">
-        <h2>what we do?</h2>
-        <div className="container">
-        <div className="row icons-row">
-        <div className="icon-holder">
-          <span>
-            <img src="img/icons/idea.webp" alt="Education" />
-          </span>
-          <h3>Exclusive educative content</h3>
+            </div>
+          </div>
         </div>
-        <div className="icon-holder">
-          <span>
-            <img src="img/icons/live-music.webp" alt="Music Industry" />
-          </span>
-          <h3>Access to the connections in music industry</h3>
+        {/* WHAT WE DO SECTION */}
+        <div
+          className="section nj-light-section"
+          data-scroll-to-top-style="dark"
+          data-cursor-style="dark"
+        >
+          <h2>what we do?</h2>
+          <div className="container">
+            <div className="row icons-row">
+              <div className="icon-holder">
+                <span>
+                  <img src="img/icons/idea.webp" alt="Education" />
+                </span>
+                <h3>Exclusive educative content</h3>
+              </div>
+              <div className="icon-holder">
+                <span>
+                  <img src="img/icons/live-music.webp" alt="Music Industry" />
+                </span>
+                <h3>Access to the connections in music industry</h3>
+              </div>
+              <div className="icon-holder">
+                <span>
+                  <img src="img/icons/celebrating.webp" alt="Fun and Joy" />
+                </span>
+                <h3>Fun and joy</h3>
+              </div>
+              <div className="icon-holder">
+                <span>
+                  <img src="img/icons/celebrating.webp" alt="Fun and Joy" />
+                </span>
+                <h3>Fun and joy</h3>
+              </div>
+              <div className="icon-holder">
+                <span>
+                  <img src="img/icons/celebrating.webp" alt="Fun and Joy" />
+                </span>
+                <h3>Fun and joy</h3>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="icon-holder">
-          <span>
-            <img src="img/icons/celebrating.webp" alt="Fun and Joy" />
-          </span>
-          <h3>Fun and joy</h3>
+        {/* TEAM SECTION */}
+        <div className="section nj-dark-section" id="team-section">
+          <div className="nj-section-header mb-5">
+            <h1 className="nj-section-header-title">The team</h1>
+          </div>
+          <div className="container team-images">
+            {galleryImages.map((image, index) => {
+              return (
+                <img
+                  onClick={() => imageClickHandler(index)}
+                  data-cursor-effect="hover"
+                  src={process.env.PUBLIC_URL + image}
+                  key={index}
+                  alt="gallery image"
+                />
+              );
+            })}
+          </div>
         </div>
-        <div className="icon-holder">
-          <span>
-            <img src="img/icons/celebrating.webp" alt="Fun and Joy" />
-          </span>
-          <h3>Fun and joy</h3>
-        </div>
-        <div className="icon-holder">
-          <span>
-            <img src="img/icons/celebrating.webp" alt="Fun and Joy" />
-          </span>
-          <h3>Fun and joy</h3>
-        </div>
-        </div>
-        </div>
-      </div>
-      {/* TEAM SECTION */}
-      <div className="section nj-dark-section" id="team-section">
-      <div className="nj-section-header mb-5">
-        <h1 className="nj-section-header-title">The team</h1>
-      </div>
-      <div className="container team-images">
-        {galleryImages.map((image, index) => {
-          return <img onClick={() => imageClickHandler(index)} data-cursor-effect="hover" src={process.env.PUBLIC_URL + image} key={index} alt="gallery image" />
-        })}
-      </div>
-      </div>
-      {/* WHO WE ARE SECTION */}
-      <div className="container mt-0">
+        {/* WHO WE ARE SECTION */}
+        <div className="container mt-0">
           <div className="nj-section-header">
             <h1 className="nj-section-header-title-team nj-mt-60 who-we-are">
               who we are
             </h1>
           </div>
         </div>
-      <div className="container">
+        <div className="container">
           <div className="row">
             <div className="col-12">
               <p className="nj-section-content">
@@ -180,7 +208,7 @@ const Volunteer = () => {
             </div>
           </div>
         </div>
-      <div className="container">
+        <div className="container">
           <div className="row fourth">
             <h3 className="mt-5 col-12" style={{ marginBottom: "25px" }}>
               Stellar Hosts:
@@ -203,7 +231,9 @@ const Volunteer = () => {
                     data-cursor-effect="hover"
                   >
                     <li>
-                    <FontAwesomeIcon icon={["fab", "facebook"]} ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        icon={["fab", "facebook"]}
+                      ></FontAwesomeIcon>
                     </li>
                   </a>
                   <a
@@ -212,7 +242,9 @@ const Volunteer = () => {
                     data-cursor-effect="hover"
                   >
                     <li>
-                    <FontAwesomeIcon icon={["fab", "youtube"] } ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        icon={["fab", "youtube"]}
+                      ></FontAwesomeIcon>
                     </li>
                   </a>
                   <a
@@ -221,7 +253,9 @@ const Volunteer = () => {
                     data-cursor-effect="hover"
                   >
                     <li>
-                    <FontAwesomeIcon icon={["fab", "instagram"]} ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        icon={["fab", "instagram"]}
+                      ></FontAwesomeIcon>
                     </li>
                   </a>
                 </ul>
@@ -258,7 +292,7 @@ const Volunteer = () => {
               </div>
             </div>
 
-{/*             <div
+            {/*             <div
               className="card mb-3 col-lg-4 col-12"
               style={{ marginTop: "0px", border: "none" }}
             >
@@ -346,7 +380,9 @@ const Volunteer = () => {
                     data-cursor-effect="hover"
                   >
                     <li>
-                    <FontAwesomeIcon icon={["fab", "twitch"]} ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        icon={["fab", "twitch"]}
+                      ></FontAwesomeIcon>
                     </li>
                   </a>
                   <a
@@ -355,7 +391,9 @@ const Volunteer = () => {
                     data-cursor-effect="hover"
                   >
                     <li>
-                    <FontAwesomeIcon icon={["fab", "youtube"] } ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        icon={["fab", "youtube"]}
+                      ></FontAwesomeIcon>
                     </li>
                   </a>
                   <a
@@ -364,7 +402,9 @@ const Volunteer = () => {
                     data-cursor-effect="hover"
                   >
                     <li>
-                    <FontAwesomeIcon icon={["fab", "instagram"]} ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        icon={["fab", "instagram"]}
+                      ></FontAwesomeIcon>
                     </li>
                   </a>
                 </ul>
@@ -377,13 +417,19 @@ const Volunteer = () => {
                 className="card-text card-text-host"
                 style={{ fontWeight: "400" }}
               >
-                "Hey all, I am Leanri Paulsen. On my social platforms, I describe myself as a music teacher, book geek, occasional hermit, 
-                and an unobstructed unconventional non-traditional utilitarian.  Music has always been a part of my existence, from early 
-                piano lessons to sponging up whichever 8-Track tape my parents would stuff into the stereo.  This addiction fueled a passion
-                 that drove me to finish a postgraduate degree in music and continue on as a music teacher in my native Namibia.  
-                 Music and the creative arts have opened many doors for me and have even led me to indulge in other outlets such as writing 
-                 and video editing (see my socials).  These days, however, you can find me streaming from my home in The Netherlands, 
-                 chilling with the furkids and lapping up interesting YouTube finds. "
+                "Hey all, I am Leanri Paulsen. On my social platforms, I
+                describe myself as a music teacher, book geek, occasional
+                hermit, and an unobstructed unconventional non-traditional
+                utilitarian. Music has always been a part of my existence, from
+                early piano lessons to sponging up whichever 8-Track tape my
+                parents would stuff into the stereo. This addiction fueled a
+                passion that drove me to finish a postgraduate degree in music
+                and continue on as a music teacher in my native Namibia. Music
+                and the creative arts have opened many doors for me and have
+                even led me to indulge in other outlets such as writing and
+                video editing (see my socials). These days, however, you can
+                find me streaming from my home in The Netherlands, chilling with
+                the furkids and lapping up interesting YouTube finds. "
               </p>
 
               <div className="nj-read-more-button-wrapper">
@@ -413,7 +459,9 @@ const Volunteer = () => {
                     data-cursor-effect="hover"
                   >
                     <li>
-                    <FontAwesomeIcon icon={["fab", "bandcamp"]} ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        icon={["fab", "bandcamp"]}
+                      ></FontAwesomeIcon>
                     </li>
                   </a>
                   <a
@@ -422,7 +470,9 @@ const Volunteer = () => {
                     data-cursor-effect="hover"
                   >
                     <li>
-                    <FontAwesomeIcon icon={["fab", "youtube"] } ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        icon={["fab", "youtube"]}
+                      ></FontAwesomeIcon>
                     </li>
                   </a>
                   <a
@@ -431,7 +481,9 @@ const Volunteer = () => {
                     data-cursor-effect="hover"
                   >
                     <li>
-                    <FontAwesomeIcon icon={["fab", "instagram"]} ></FontAwesomeIcon>
+                      <FontAwesomeIcon
+                        icon={["fab", "instagram"]}
+                      ></FontAwesomeIcon>
                     </li>
                   </a>
                 </ul>
@@ -444,12 +496,16 @@ const Volunteer = () => {
                 className="card-text card-text-host"
                 style={{ fontWeight: "400" }}
               >
-                "Roland Stolk began playing piano at the age of eight and drums at ten. He studied drums at the
-Elementary School for Ballet and Music in Rotterdam, where he discovered the vibraphone. At
-sixteen, he began studying vibraphone and drums at the Rotterdam Conservatory in Rotterdam
-during which time he followed workshops by Milt Jackson and Dave Samuels. Roland graduated with
-honers from the Rotterdam Conservatory in 1987. In 1989, Roland continued his study at the Berklee
-College of Music in Boston, Massachusetts, USA, he studied under Ed Saindon and Gary Burton."
+                "Roland Stolk began playing piano at the age of eight and drums
+                at ten. He studied drums at the Elementary School for Ballet and
+                Music in Rotterdam, where he discovered the vibraphone. At
+                sixteen, he began studying vibraphone and drums at the Rotterdam
+                Conservatory in Rotterdam during which time he followed
+                workshops by Milt Jackson and Dave Samuels. Roland graduated
+                with honers from the Rotterdam Conservatory in 1987. In 1989,
+                Roland continued his study at the Berklee College of Music in
+                Boston, Massachusetts, USA, he studied under Ed Saindon and Gary
+                Burton."
               </p>
 
               <div className="nj-read-more-button-wrapper">
@@ -464,8 +520,8 @@ College of Music in Boston, Massachusetts, USA, he studied under Ed Saindon and 
             </div>
           </div>
         </div>
-      {/* JOIN US SECTION */}
-      <div
+        {/* JOIN US SECTION */}
+        <div
           className="section nj-light-section nj-pdt-60"
           id="nj-single-project-section"
           data-scroll-to-top-style="dark"
@@ -490,16 +546,18 @@ College of Music in Boston, Massachusetts, USA, he studied under Ed Saindon and 
                 className="black-button"
                 href="mailto:stellarsoundpodcast@gmail.com"
                 data-cursor-effect="exclusion"
-                onClick={() => gaEventTracker("become a volunteer volunteer page")}
+                onClick={() =>
+                  gaEventTracker("become a volunteer volunteer page")
+                }
               >
                 become a volunteer
               </a>
             </div>
           </div>
         </div>
-    </div>
+      </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Volunteer
+export default Volunteer;
