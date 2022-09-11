8bb716e1be93a6c7df973520e7c775e1b9554226
zorana.lazarevic <zorana.lazarevic@basic-fit.com>
2022-09-11 22:44:52 +0200

    added episodes page

---
+++ src/allEpisodesData.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ep1 from "../src/assets/NewCoverIMG/1.jpg";
+
+function episodesData() {
+  return [
+    {
+      typeOfEpisode: "Stellar Season 1",
+      episodePath: "episode-1",
+      episode: "1",
+      imgPath: ep1,
+      altText: "Stellar Sound Podcast Pilot: Episode 1",
+      guest: `Leo from "Just Leo"`,
+      description: `Creative process, autism and mental health of a musician,
+        style and uniqueness of the autor's work, irish bouzouki and
+        many more!`,
+    },
+  ];
+}
+
+export default episodesData;
