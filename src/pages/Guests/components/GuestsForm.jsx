0f2dea3516c531dc34fb93d190c7d1bfe71ca488
zorana.lazarevic <zorana.lazarevic@basic-fit.com>
2022-09-11 18:29:56 +0200

    added form for guests

--- src/pages/Guests/components/GuestsForm.jsx
+++ src/pages/Guests/components/GuestsForm.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import useAnalyticsEventTracker from "../../../useAnalyticsEventTracker";
 
 library.add(fab);
 
 export default function GuestsForm() {
+  const gaEventTracker = useAnalyticsEventTracker("GuestsForm");
+
   return (
     <div className="contact">
       <div className="form w-100 mb-3">
@@ -14,7 +17,7 @@ export default function GuestsForm() {
             <form
               id="contact-form"
               className="contact-form"
-              action="https://formspree.io/f/mjvjagld"
+              action="https://formspree.io/f/meqdevld"
               method="POST"
             >
               <div className="form-group">
@@ -53,8 +56,8 @@ export default function GuestsForm() {
                 <input
                   className="contact-form-text"
                   type="text"
-                  id="link"
-                  name="link"
+                  id="Link"
+                  name="Link"
                   required
                 />
               </div>
@@ -96,7 +99,7 @@ export default function GuestsForm() {
               </div>
 
               <div className="form-group">
-                <label htmlFor="history">
+                <label>
                   Write about your music history
                   <span style={{ color: "red" }}>*</span>
                 </label>
@@ -116,23 +119,185 @@ export default function GuestsForm() {
               <div className="form-group">
                 <label htmlFor="avoid">
                   Any topics you would like to avoid
+                  <span style={{ color: "red" }}>*</span>
                 </label>
                 <input
                   className="contact-form-text"
                   type="text"
-                  id="avoid"
-                  name="avoid"
+                  id="Avoid"
+                  name="Avoid"
                   required
                 />
               </div>
 
+              {/* song breakdown section */}
+              <div className="form-group mt-5">
+                <h5>“Song breakdown” rubric</h5>
+
+                <label>
+                  In this rubric you are showcasing one of your songs. You
+                  should as a tutorial analyze one of your songs and explain
+                  production, progressions, timbre, lyrics and any other
+                  technical choices you prefer. This video will be available
+                  exclusively to our Discord server “Stellar Sound Community”
+                  and has educational purposes. This rubric is mandatory to all
+                  our guests.
+                </label>
+                <label>
+                  Chose one: <span style={{ color: "red" }}>*</span>
+                </label>
+                <label style={{ display: "flex" }}>
+                  <input
+                    type="radio"
+                    name="Song breakdown"
+                    value="I will record song breakdown on my own and send via mail."
+                    required
+                  />
+                  <div className="ml-3 text-left">
+                    Record at home your screen and showcase a DAW project of one
+                    of your songs in the duration of 15-20 minutes and send to
+                    stellarsoundpodcast@gmail.com
+                  </div>
+                </label>
+
+                <label style={{ display: "flex" }}>
+                  <input
+                    type="radio"
+                    name="Song breakdown"
+                    value="Verbaly ask me questions for this rubric during the interview."
+                    required
+                  />
+                  <div className="ml-3 text-left">
+                    We verbally ask questions during the interview. There is no
+                    special preparation needed from your side, you will just on
+                    spot answer questions.
+                  </div>
+                </label>
+              </div>
+              {/* song breakdown section - end */}
+
+              {/* galactic trinity section */}
+              <div className="form-group mt-5">
+                <h5>Consider “Galactic Trinity” rubric (optional)</h5>
+
+                <label>
+                  Prepare the 3 of your collection to showcase any 3 objects
+                  that mean something special to you. Your favorite instrument,
+                  records, gadgets, anything that inspired your music journey…
+                </label>
+                <label>
+                  Chose one: <span style={{ color: "red" }}>*</span>
+                </label>
+                <label style={{ display: "flex" }}>
+                  <input
+                    type="radio"
+                    name="galactic-trinity"
+                    value="Yes, I want to prepare 3 items for the Galactic Trinity"
+                    required
+                  />
+                  <div className="ml-3 text-left">
+                    Yes I will prepare 3 objects of my choice
+                  </div>
+                </label>
+
+                <label style={{ display: "flex" }}>
+                  <input
+                    type="radio"
+                    name="galactic-trinity"
+                    value="No I want to skip Galactic Trinity rubric"
+                    required
+                  />
+                  <div className="ml-3 text-left">
+                    I want to skip this rubric
+                  </div>
+                </label>
+              </div>
+              {/* galactic trinity section - end */}
+
+              {/* the interview confirm terms */}
+              <div className="form-group mt-5 mb-5">
+                <h5>The Interview</h5>
+                <label>
+                  The Interview is recorded via video call. It will be a
+                  relaxed, non-formal chat in a spontaneous discussion. The
+                  recording will last up to 2 hours and will be recorded in a
+                  video format. We suggest to use your best microphone, plan
+                  your room background (we love you showing off your music
+                  studio), plan the light source to be the opposite of you
+                </label>
+                <h5 className="mt-5">Riverside</h5>
+                <label>
+                  For recording we are using an in-browser platform called
+                  Riverside. This platform is recording your video & audio via
+                  browser and it's a phenomenal tool! You will receive an email
+                  invitation with a link before the interview. We are suggesting
+                  using any browser except Safari, as we have faced multiple
+                  technical issues. Also, this platform requires fast internet.
+                  Make sure you find a place with a good protocol. More on:{" "}
+                  <a href="https://riverside.fm/">Riverside Website</a>
+                </label>
+                <h5 className="mt-5">Stellar Sound Foundation</h5>
+                <label>
+                  Under the roof of the Stellar Sound Foundation, we are a
+                  non-profit organization operating by donations and
+                  partnerships in order to create diverse projects in the field
+                  of Music, Technology and Art. More on:{" "}
+                  <a href="www.stellarsoundfoundation.com">
+                    Stellar Sound Foundation Website
+                  </a>
+                </label>
+
+                <h5 className="mt-5">Stellar Volunteers</h5>
+                <label>
+                  We are an international group of musicians and music
+                  enthusiasts working online. We are always open for new
+                  volunteers. Help us find new volunteers by promoting on our
+                  social media. More on:{" "}
+                  <a href="https://www.stellarsoundpodcast.com/become-a-volunteer">
+                    Become a volunteer
+                  </a>
+                </label>
+                <h5 className="mt-5">Stellar Sound Podcast</h5>
+                <label>
+                  Stellar Sound Podcast is a podcast here to empower, encourage
+                  and promote creative musicians. We are available on all
+                  podcast streaming services like Spotify, Apple Podcasts,
+                  Google Podcasts, as well as on Youtube including video. Find
+                  out more on our website:{" "}
+                  <a href="www.stellarsoundpodcast.com">
+                    Stellar Sound Podcast Website
+                  </a>
+                </label>
+                <h5 className="mt-5">Partnerships</h5>
+                <label>
+                  To all organizations and potential partners contact us on:{" "}
+                  <a href="mailto:stellarsoundpartnerships@gmail.com">
+                    Partnerships contact
+                  </a>
+                </label>
+
+                <label style={{ display: "flex" }}>
+                  <input
+                    type="checkbox"
+                    name="checkbox"
+                    value="I read and understood how the interview will go."
+                    required
+                  />
+                  <div className="ml-3">
+                    I have read and agree with the above information
+                    <span style={{ color: "red" }}>*</span>
+                  </div>
+                </label>
+              </div>
+              {/* the interview confirm terms - end */}
+
               <div className="container mt-3">
                 <div className="row center">
                   <button
-                    // onClick={() => gaEventTracker("Send contact form")}
+                    onClick={() => gaEventTracker("Send guests form")}
                     type="submit"
                     data-cursor-effect="exclusion"
-                    className="text-center "
+                    className="text-center"
                   >
                     Send
                   </button>
